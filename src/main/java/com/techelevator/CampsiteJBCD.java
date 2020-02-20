736d92aaf9835df9d234d2d4089df208e3ce784b
Jarret Fawcett <jarret.fawcett@gmail.com>
2020-02-20 12:53:08 -0500

    Added stuff

---
+++ src/main/java/com/techelevator/CampsiteJBCD.java
@@ -0,0 +1,25 @@
+package com.techelevator;
+
+import java.util.List;
+
+public class CampsiteJBCD implements CampsiteDAO {
+
+	@Override
+	public List<Campsite> getAllSites() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Campsite findSiteById(int siteId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Campsite> findSitesByCampgroundId(int campgroundId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/techelevator/ReservationJBCD.java
@@ -0,0 +1,26 @@
+package com.techelevator;
+
+import java.util.Date;
+import java.util.List;
+
+public class ReservationJBCD implements ReservationDAO {
+
+	@Override
+	public List<Reservation> getAllReservations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation findRezById(int rezId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createRez(int siteId, String rezName, Date fromDate, Date toDate) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
