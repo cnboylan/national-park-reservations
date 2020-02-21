51c846ceded3ddbd47b9360f05fbc6eb5449d8b0
Jarret Fawcett <jarret.fawcett@gmail.com>
2020-02-21 14:37:35 -0500

    Homeostasis 98%

--- src/main/java/com/techelevator/CampgroundCLI.java
+++ src/main/java/com/techelevator/CampgroundCLI.java
@@ -1,7 +1,9 @@
 package com.techelevator;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
-
+import java.util.Date;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -13,7 +15,6 @@ public class CampgroundCLI {
 	private ReservationDAO reservationDAO;
 // TO-DO Build tests.
 // TO-DO Build CLI
-
 
 	public static void main(String[] args) {
 		BasicDataSource dataSource = new BasicDataSource();
@@ -37,57 +38,106 @@ public class CampgroundCLI {
 		mainMenu();
 	}
 
-	public void mainMenu(){
+	public void mainMenu() {
 		// ********** INPUT VALIDATION **************
 		// *****Don't let Tom break your CLI again***
 
-		// Select from:
-		//         ---1. View All Parks
-		//         ---2. Select park from All Parks
-		//         ---3. Select Campground from above park
-		//         ---4. Select campsite from above campground
-		//         ---5. Top 5 Campsites in no particular order ?
-		//         ---6. Bonus - off season date returns 0 campsites
-		//		   ---7. Double Bonus - Advanced search to filter campsite amenities
-		//		   ---8. Book reservation at available campsite
-		// 					- Requires name for reservation, start date, end date
-		// 					- Returns confirmation ID once the reservation has been submitted
-		//         ---9. Double Super Bonus - Select park and search campsite availability within
-		// 					park to make a reservation.
-		// 					- Return up to 5 campsites for each campground along with daily cost or duration cost?
-		//         ---10. ULTRA BONUS - View list of all upcoming reservations within 30 days.
-			while (true) {
-				Scanner scanner = new Scanner(System.in);
-				
-				System.out.println("View parks interface.");
-				System.out.println("Select a park for further details: ");
-				parkDAO.printParkNames();
-				System.out.println("0 - Quit");
-				int parkChoice = Integer.parseInt(scanner.nextLine());
-				
-				// add input validation sometime in the future maybe never.
-				
-				if (parkChoice == 0) {
-					break;
-				}
-				System.out.println("Park information screen.");
-				parkDAO.printParkInfo(parkChoice);
-				
-				System.out.println("Select one of the following options: ");
-				System.out.println("1 - View Campgrounds");
-				System.out.println("2 - Search for Reservation");
-				System.out.println("3 - Return to Previous Screen");
-				int userChoice = Integer.parseInt(scanner.nextLine());
-				
-				if (userChoice == 1) {
+		// ---5. Top 5 Campsites in no particular order ?
+		// ---6. Bonus - off season date returns 0 campsites
+		// ---7. Double Bonus - Advanced search to filter campsite amenities
+		// ---8. Book reservation at available campsite
+		// - Requires name for reservation, start date, end date
+		// - Returns confirmation ID once the reservation has been submitted
+		// ---9. Double Super Bonus - Select park and search campsite availability
+		// within
+		// park to make a reservation.
+		// - Return up to 5 campsites for each campground along with daily cost or
+		// duration cost?
+		// ---10. ULTRA BONUS - View list of all upcoming reservations within 30 days.
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("View parks interface.");
+			System.out.println("Select a park for further details: ");
+			parkDAO.printParkNames();
+			System.out.println("0 - Quit");
+			int parkChoice = Integer.parseInt(scanner.nextLine());
+
+			// add input validation sometime in the future maybe never.
+
+			if (parkChoice == 0) {
+				break;
+			}
+			System.out.println("PARK INFORMATION SCREEN");
+			parkDAO.printParkInfo(parkChoice);
+
+			System.out.println("Select one of the following options: ");
+			System.out.println("1 - View Campgrounds");
+			System.out.println("2 - Search for Reservation");
+			System.out.println("3 - Return to Previous Screen");
+			int userChoice = Integer.parseInt(scanner.nextLine());
+
+			if (userChoice == 1) {
+				while (true) {
+					System.out.println("Park Campgrounds:");
 					System.out.println("Name\tOpen\tClose\tDaily Fee");
 					campgroundDAO.printCampgroundInfo(userChoice);
-					
-				} else if (userChoice == 2) {
-					
+					System.out.println("Select an option: ");
+					System.out.println("1. Search for available reservation.");
+					System.out.println("2. Return to previous screen.");
+					int resMenuChoice = Integer.parseInt(scanner.nextLine());
+					if (resMenuChoice == 1) {
+						// which campground
+						// which dates
+						while (true) {
+							System.out.println("Select a campground from the menu:");
+							campgroundDAO.printCampgroundInfo(userChoice);
+							int campgroundID = Integer.parseInt(scanner.nextLine());
+							String startDate;
+							String endDate;
+							Date start_date = null;
+							Date end_date = null;
+							while (true) {
+								System.out.println("Enter a start date for your reservation. (Format: yyyy-mm-dd)");
+								startDate = scanner.nextLine();
+								String pattern = "yyyy-MM-dd";
+								SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+								try {
+									start_date = sdf.parse(startDate);
+								} catch (ParseException e) {
+									System.out.println("Your start date input is just plain wrong.");
+									System.out.println("******************************************");
+									System.out.println("You are returning to the campground menu.");
+									// e.printStackTrace();
+									break;
+								}
+								System.out.println("Enter an end date for your reservation. (Format: yyyy-mm-dd)");
+								endDate = scanner.nextLine();
+								try {
+									end_date = sdf.parse(endDate);
+								} catch (ParseException e) {
+									System.out.println("Your end date input is just plain wrong.");
+									System.out.println("****************************************");
+									System.out.println("You are returning to the campground menu.");
+
+									// e.printStackTrace();
+									break;
+								}
+							}
+						campsiteDAO.getAvailableSites(campgroundID, start_date, end_date);
+						
+						}
+						// String pattern = "yyyy-MM-dd";
+						// SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+						// Date date = simpleDateFormat.parse("2018-09-09");
+					}
+
 				}
-				
-				
+
+			} else if (userChoice == 2) {
+
 			}
+
+		}
 	}
 }
--- src/main/java/com/techelevator/CampsiteDAO.java
+++ src/main/java/com/techelevator/CampsiteDAO.java
@@ -1,5 +1,6 @@
 package com.techelevator;
 
+import java.util.Date;
 import java.util.List;
 
 public interface CampsiteDAO {
@@ -10,4 +11,6 @@ public interface CampsiteDAO {
 	public void createCampsite(Campsite newCampsite);
 	public void updateCampsite(Campsite updatedCampsite);
 	public void deleteCampsite(Campsite deletedCampsite);
+	public List<Campsite> getAvailableSites(int campgroundId, Date fromDate, Date toDate);
+	
 }
